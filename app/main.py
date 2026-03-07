dd369784d5a9104f620d9362f13f5586c04dd815
Hamza Rashid <hamzarashid@Hamzas-MacBook-Air.local>
2026-03-07 01:17:54 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -14,6 +14,13 @@ def main():
         elif command[:4] == "echo":
             sys.stdout.write(f"{command[5:]}\n")
 
+        elif command.startswith("type"):
+             
+            if command[5:] in ['echo', 'exit', 'type']:
+                print(f"{command[5:]} is a shell builtin")
+            else:
+                print(f"{command[5:]}: not found")
+
         else:
             print(f"{command}: command not found")
 
