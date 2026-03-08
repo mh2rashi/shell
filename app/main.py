dc18df85d5820b3ab949e5d266f4a4c6b4449a3d
Hamza Rashid <hamzarashid@Hamzas-MacBook-Air.local>
2026-03-07 23:19:39 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import sys
-
+import os
 
 def main():
 
@@ -18,6 +18,18 @@ def main():
              
             if command[5:] in ['echo', 'exit', 'type']:
                 print(f"{command[5:]} is a shell builtin")
+                continue
+
+            paths = os.environ['PATH'].split(os.pathsep)
+
+            for path in paths:
+
+                full_path = os.path.join(path, command[5:])
+
+                if os.access(full_path, os.X_OK):
+                    print(f"{command[5:]} is {full_path}")
+                    break
+
             else:
                 print(f"{command[5:]}: not found")
 
